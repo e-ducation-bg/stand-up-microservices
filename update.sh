b24bf2f40aa86fafddb90167dbba547d83052d01
justitsi <hristob1999@gmail.com>
2021-11-19 18:16:30 +0000

    Fix WSL2 docker-compose error

--- update.sh
+++ update.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # Update repos
-cd src/node-image-compression
-git pull
-cd ../jwt_issuer_public
-git pull
-cd ../frontend
-git pull
-cd ../microservice-reverse-proxy
-git pull
-cd ../flask-permission-manager
-git pull
-cd ../api
-git pull
+cd src
+# delete repos
+rm -rf node-image-compression
+rm -rf api
+rm -rf frontend
+rm -rf jwt_issuer_public
+rm -rf microservice-reverse-proxy
+rm -rf flask-permission-manager
+
+# re-download them
+git clone git@github.com:e-ducation-bg/node-image-compression.git
+git clone git@github.com:e-ducation-bg/jwt_issuer_public.git
+git clone git@github.com:e-ducation-bg/microservice-reverse-proxy.git
+git clone git@github.com:e-ducation-bg/flask-permission-manager.git
+git clone git@github.com:e-ducation-bg/frontend.git
+git clone git@github.com:e-ducation-bg/api.git
 
 # return to main dir
-cd ../..
+cd ..
 
 # Update images
 sudo docker-compose build --no-cache --parallel
